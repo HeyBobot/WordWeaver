1320d623467a399a6a9ecc57183af99d95a8d305
HeyBobot <hey.bobot@gmail.com>
2024-08-02 20:39:54 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+
+$(function() {
+WTF.init({
+     heading: [
+             "feyral says:"
+         ],
+         response: [
+             "another one!"
+     ],
+         template: [
+             "hello my name is feyral starweaver, your friendly neighborhood @adjective @noun",
+             "chat i'm @verb",
+             "i'm also the aspect of @noun",
+             "ooooh, @noun!",
+             "there's some @plural in this house",
+             "my chat is full of @plural",
+             "we got that hotel motel @noun @noun",
+             "you know, like, @noun",
+             "i love @plural",
+             "give me your @noun",
+             "clicking this will take you straight to @noun central",
+             "today, i'm going to be @verb",
+             "never stop @verb",
+        ],
+         noun: [
+             "love",
+             "catgirl",
+             "streamer",
+             "stinkbug",
+             "guh",
+             "nya",
+             "disaster",
+             "meat",
+             "pussy",
+             "chat",
+             "yaoi",
+             "minecraft",
+             "pokemon",
+             "fortnite",
+         ],
+         adjective: [
+             "cunty",
+             "fucking",
+             "wet-ass",
+             "certified",
+             "gay",
+             "submissive",
+             "big",
+             "zooted",
+         ],
+         verb: [
+            "gaming",
+            "streaming",
+            "slaying",
+            "pulling out",
+            "jiggling",
+            "fucking",
+            "jorking it",
+        ],
+        plural: [
+            "whores",
+            "balls",
+            "bottoms",
+            "boobs",
+            "freaks",
+            "losers",
+        ],
+     });
+});
